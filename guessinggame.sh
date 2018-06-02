f0bd559a73dbde30b987609441fbafe11b30871e
Nevena <557.2015@fink.rs>
2018-06-02 11:48:57 +0200

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function ask {
+	echo "Koliko mislis da ima fajlova ovde?"
+	read koliko
+    files=$(ls -1 | wc -l)
+}
+ask
+while [[ $koliko -ne $files ]]
+do
+	if [[ $koliko -lt $files ]] 
+	then
+		echo "Sto mislis da ima toliko malo?Ima vise. "	
+	else
+		echo "Sto mislis da ima toliko mnogo? Ima manje. "	
+	fi
+	ask
+done
+echo "BRAVO, pogodio si!"
+
+echo "---" && ls -1
